5ecf0dce5812fb81dd33eb761ef4328141fa3452
Jay <75156028+jay-1072@users.noreply.github.com>
2022-05-03 13:52:24 +0530

    Add files via upload

---
+++ Warden/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<title>Hostel Portal</title>
+		<link rel="stylesheet" type="text/css" href="../css/headerFooter.css">
+		<link rel="stylesheet" type="text/css" href="../css/loginPage.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+		<script
+		src="https://code.jquery.com/jquery-3.6.0.js"
+		integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+		crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="footer">
+			<p>Design & Developed by Information Technology Department</p>
+		</div>
+	</body>
+</html>
