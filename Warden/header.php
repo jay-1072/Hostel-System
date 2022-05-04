c5fc6fc80562ab4dde9a06a352ae9f8cb1b30f12
Shubham <dusaneshubham>
2022-05-04 20:29:41 +0530

    5 report tables created

--- Warden/header.php
+++ Warden/header.php
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-		<title>Hostel Portal</title>
-		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
-		<script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-		<div class="top">
-			<div class="imgBx">
-				<a href="#" class="logo"><img src="../images/clogo.png" class="rounded float-start mx-3 my-3" style="height: 100px; width: 100px;"></a>
-			</div>
-			<div class="cname d-grid gap-0">
-				<h1 class="h1 mt-3">Vishwakarma Government Engineering College</h1>
-				<h3 class="h3">Chandkheda, Ahmedabad</h3>
-			</div>
-		</div>
-	</body>
-=======
 <!DOCTYPE html>
 <html>
 	<head>
@@ -40,5 +18,4 @@
 			</div>
 		</div>
 	</body>
->>>>>>> 05c0185cda994544fa6060b367d2795b0e0d01a0
 </html>
--- details/feespaymentdetails.php
+++ details/feespaymentdetails.php
@@ -1,67 +1,111 @@
+<?php
+
+$hostelName = $_GET['name'];
+$fullHostelName = '';
+
+switch ($hostelName) {
+	case 'BH-1':
+		$fullHostelName = 'Boys Hostel 1';
+		break;
+	case 'BH-2':
+		$fullHostelName = 'Boys Hostel 2';
+		break;
+	case 'GH':
+		$fullHostelName = 'Girls Hostel';
+		break;
+}
+?>
+
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Hostel Portal</title>
-	</head>
-	<body>
-		<?php
-		include("../warden/header.php");
-		?>
-		<hr />
-		<div class="container-fluid">
-			<div class="row">
-				<h3 class="text-center mt-3 mb-2 text-primary">Paid fees details</h3>
-				<div class="table-responsive">
-					<table class="table table-bordered table-hover mt-3 align-middle">
-						<thead>
-							<tr class="">
-								<th>Room Number</th>
-								<th>Enrollment Number</th>
-								<th>Name</th>
-								<th>Branch</th>
-								<th>Semester</th>
-								<th>Course</th>
-								<th>DU Referencre Number</th>
-								<th>Payment Date</th>
-								<th>Amount</th>
-								<th>Penalty</th>
-								<th>Receipt</th>
-								<th>Status</th>
-								<th>Remarks</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th scope="row">1101</th>
-								<td>190170116000</td>
-								<td>Xyz</td>
-								<td>IT</td>
-								<td>6</td>
-								<td>BE/ME</td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td>Approve / Reject</td>
-								<td></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-			<div class="col-12 text-center">
-				<button type="submit" class="btn btn-info">Generate Report</button>
-				<a href="../warden/report.php" class="mx-3 text-decoration-none">
-					Back
-				</a>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hostel Portal</title>
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+</head>
+
+<body>
+	<?php
+	include("../warden/header.php");
+	?>
+	<hr />
+	<div class="container-fluid">
+		<div class="row">
+			<h3 class="text-center mt-3 mb-2 text-primary">Paid fees details</h3>
+			<h5 class="text-center mt-3 mb-2 text-primary"><?php echo $fullHostelName; ?></h5>
+			<div class="table-responsive">
+				<table class="table table-bordered table-hover table-striped mt-3 align-middle text-center" id="myTable">
+					<thead>
+						<tr class="">
+							<th>Room Number</th>
+							<th>Enrollment Number</th>
+							<th>Name</th>
+							<th>Branch</th>
+							<th>Semester</th>
+							<th>Course</th>
+							<th>DU Reference Number</th>
+							<th>Payment Date</th>
+							<th>Amount</th>
+							<th>Penalty</th>
+							<th>Receipt</th>
+							<th>Status</th>
+							<th>Remarks</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						include "../dbConn.php";
+						$sql = "SELECT * FROM hostel_student_details as hsd JOIN fees ON hsd.enrollment_no = fees.enrollment_no AND hsd.hostel_name = ? ORDER BY fees.payment_date DESC";
+						$stmt = $conn->prepare($sql);
+						$stmt->bind_param("s", $hostelName);
+						$stmt->execute();
+						$result = $stmt->get_result();
+						if ($result->num_rows > 0) {
+							while ($row = $result->fetch_assoc()) {
+								echo '<tr>
+									<th scope="row">' . $row['room_no'] . '</th>
+									<td>' . $row['enrollment_no'] . '</td>
+									<td>XYZ</td>
+									<td>IT</td>
+									<td>6</td>
+									<td>BE/ME</td>
+									<td>' . $row['DU_reference_no'] . '</td>
+									<td>' . $row['payment_date'] . '</td>
+									<td>' . $row['amount_paid'] . '</td>
+									<td>' . $row['penalty'] . '</td>
+									<td>Receipt</td>
+									<td>' . $row['status'] . '</td>
+									<td>' . $row['remarks'] . '</td>
+								</tr>';
+							}
+						}
+						?>
+					</tbody>
+				</table>
 			</div>
 		</div>
-		<p>To do: Hostel name remaining</p>
-		<?php
-		include("../warden/footer.php");
-		?>
-	</body>
+		<div class="col-12 text-center">
+			<button type="submit" class="btn btn-info">Generate Report</button>
+			<a href="../warden/report.php" class="mx-3 text-decoration-none">
+				Back
+			</a>
+		</div>
+	</div>
+	<p>To do: Hostel name remaining</p>
+	<?php
+	include("../warden/footer.php");
+	?>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
+<script>
+	$(document).ready(function() {
+		$('#myTable').DataTable();
+	});
+</script>
+
 </html>
